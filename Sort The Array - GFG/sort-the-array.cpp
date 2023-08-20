d6da64228c70109f61ffa77dc2c68d279f4b182d
Sushmita Majumder <109573354+binarybeauty45@users.noreply.github.com>
2023-08-20 22:20:10 +0530

    Added solution - LeetHub

--- Sort The Array - GFG/sort-the-array.cpp
+++ Sort The Array - GFG/sort-the-array.cpp
@@ -6,8 +6,10 @@ using namespace std;
 class Solution{
     public:
     vector<int> sortArr(vector<int>arr, int n){
-        sort(arr.begin(), arr.end());
-        return arr;
+    //complete the function here
+    // sort(arr.begin(), arr.end());
+    sort()
+    return arr;
     }
 };
 
